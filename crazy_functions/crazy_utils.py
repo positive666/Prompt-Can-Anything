654e4bf4944065cd31b804263882f235449ad05e
positive666 <286040359@qq.com>
2023-08-08 17:14:17 +0800

    update

--- crazy_functions/crazy_utils.py
+++ crazy_functions/crazy_utils.py
@@ -702,8 +702,15 @@ class knowledge_archive_interface():
 def try_install_deps(deps):
     for dep in deps:
         import subprocess, sys
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', dep])
+        import platform
+        system = platform.system()
 
+        # 判断系统是否为Windows
+        if system == "Windows":
+            print("Windows please  conda install pycocotools ")
+            subprocess.check_call ([sys.executable, 'conda', 'install',  'pycocotools'])
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', dep])
+        
 
 class construct_html():
     def __init__(self) -> None:
@@ -751,3 +758,57 @@ class construct_html():
         with open(f'./gpt_log/{file_name}', 'w', encoding='utf8') as f:
             f.write(self.html_string.encode('utf-8', 'ignore').decode())
 
+
+from typing import List, Tuple, Any, Union
+from langchain.schema import AgentAction, AgentFinish
+from langchain.agents import BaseSingleActionAgent
+from langchain import LLMChain, PromptTemplate
+from langchain.base_language import BaseLanguageModel
+
+
+class IntentAgent(BaseSingleActionAgent):
+    tools: List
+    llm: BaseLanguageModel
+    intent_template: str = """
+    现在有一些意图，类别为{intents}，你的任务是理解用户问题的意图，并判断该问题属于哪一类意图。
+    回复的意图类别必须在提供的类别中，并且必须按格式回复：“意图类别：<>”。
+    
+    举例：
+    问题：什么是游戏角色皮卡丘？
+    意图类别：游戏角色信息查询
+    
+    问题：什么是演员刘德华？
+    意图类别：演员信息查询
+
+    问题：“{query}”
+    """
+    prompt = PromptTemplate.from_template(intent_template)
+    llm_chain: LLMChain = None
+
+    def get_llm_chain(self):
+        if not self.llm_chain:
+            self.llm_chain = LLMChain(llm=self.llm, prompt=self.prompt)
+
+    def choose_tools(self, query) -> List[str]:
+        self.get_llm_chain()
+        tool_names = [tool.name for tool in self.tools]
+        resp = self.llm_chain.predict(intents=tool_names, query=query)
+        select_tools = [(name, resp.index(name)) for name in tool_names if name in resp]
+        select_tools.sort(key=lambda x:x[1])
+        return [x[0] for x in select_tools]
+
+    @property
+    def input_keys(self):
+        return ["input"]
+
+    def plan(
+            self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[AgentAction, AgentFinish]:
+        # only for single tool
+        tool_name = self.choose_tools(kwargs["input"])[0]
+        return AgentAction(tool=tool_name, tool_input=kwargs["input"], log="")
+
+    async def aplan(
+            self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[List[AgentAction], AgentFinish]:
+        raise NotImplementedError("IntentAgent does not support async")
