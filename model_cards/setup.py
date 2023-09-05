2ed7d78f1e838b2d184c623ff99909585a29ac50
unknown <286040359@qq.com>
2023-09-05 21:14:01 +0800

    support windows

--- model_cards/setup.py
+++ model_cards/setup.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2023 The IDEA Authors. All rights reserved.
+# Copyright 2022 The IDEA Authors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -7,6 +7,18 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------------------------
+# Modified from
+# https://github.com/fundamentalvision/Deformable-DETR/blob/main/models/ops/setup.py
+# https://github.com/facebookresearch/detectron2/blob/main/setup.py
+# https://github.com/open-mmlab/mmdetection/blob/master/setup.py
+# https://github.com/Oneflow-Inc/libai/blob/main/setup.py
+# ------------------------------------------------------------------------------------------------
 
 import glob
 import os
@@ -16,8 +28,9 @@ import torch
 from setuptools import find_packages, setup
 from torch.utils.cpp_extension import CUDA_HOME, CppExtension, CUDAExtension
 
-version = "v1"
-package_name = "Prompt-Can-Anything"
+# groundingdino version info
+version = "0.1.0"
+package_name = "groundingdino"
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -28,6 +41,12 @@ except Exception:
     pass
 
 
+def write_version_file():
+    version_path = os.path.join(cwd, "groundingdino", "version.py")
+    with open(version_path, "w") as f:
+        f.write(f"__version__ = '{version}'\n")
+        # f.write(f"git_version = {repr(sha)}\n")
+
 
 requirements = ["torch", "torchvision"]
 
@@ -35,11 +54,9 @@ torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
 
 
 def get_extensions():
- 
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    print("build path:",this_dir)
-    extensions_dir = os.path.join(this_dir, 'model_cards',"groundingdino", "models", "GroundingDINO", "csrc")
-    print("extensions_dir path:",extensions_dir)
+    extensions_dir = os.path.join(this_dir, "groundingdino", "models", "GroundingDINO", "csrc")
+
     main_source = os.path.join(extensions_dir, "vision.cpp")
     sources = glob.glob(os.path.join(extensions_dir, "**", "*.cpp"))
     source_cuda = glob.glob(os.path.join(extensions_dir, "**", "*.cu")) + glob.glob(
@@ -48,12 +65,20 @@ def get_extensions():
 
     sources = [main_source] + sources
 
+    # We need these variables to build with CUDA when we create the Docker image
+    # It solves https://github.com/IDEA-Research/Grounded-Segment-Anything/issues/53
+    # and https://github.com/IDEA-Research/Grounded-Segment-Anything/issues/84 when running
+    # inside a Docker container.
+    am_i_docker = os.environ.get('AM_I_DOCKER', '').casefold() in ['true', '1', 't']
+    use_cuda = os.environ.get('BUILD_WITH_CUDA', '').casefold() in ['true', '1', 't']
+
     extension = CppExtension
 
     extra_compile_args = {"cxx": []}
     define_macros = []
 
-    if CUDA_HOME is not None and (torch.cuda.is_available() or "TORCH_CUDA_ARCH_LIST" in os.environ):
+    if (torch.cuda.is_available() and CUDA_HOME is not None) or \
+            (am_i_docker and use_cuda):
         print("Compiling with CUDA")
         extension = CUDAExtension
         sources += source_cuda
@@ -71,7 +96,6 @@ def get_extensions():
         return None
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
-  
     include_dirs = [extensions_dir]
 
     ext_modules = [
@@ -83,7 +107,7 @@ def get_extensions():
             extra_compile_args=extra_compile_args,
         )
     ]
-   
+
     return ext_modules
 
 
@@ -168,24 +192,23 @@ def parse_requirements(fname="requirements.txt", with_version=True):
 if __name__ == "__main__":
     print(f"Building wheel {package_name}-{version}")
 
+
+    write_version_file()
+
     setup(
-        name="Prompt-Can-Anything",
-        version="0.1",
-        author="positive666",
-        description="muti-models",
-        #license=license,
+        name="groundingdino",
+        version="0.1.0",
+        author="International Digital Economy Academy, Shilong Liu",
+        url="https://github.com/IDEA-Research/GroundingDINO",
+        description="open-set object detector",
+      #  license=license,
         install_requires=parse_requirements("requirements.txt"),
         packages=find_packages(
             exclude=(
                 "configs",
                 "tests",
-                'notebooks',
             )
         ),
-         extras_require={
-        "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime"],
-        "dev": ["flake8", "isort", "black", "mypy"],
-    },  
         ext_modules=get_extensions(),
         cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
     )
